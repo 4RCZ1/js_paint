a8e957017b96e15f52cf46d11c121a2ba7801316
4RCZ1 <4RCZ1@users.noreply.github.com>
2021-06-16 15:41:10 +0200

    reworked timeline

--- timeline.js
+++ timeline.js
@@ -12,26 +12,22 @@ let keyframes = [],
     playTimer,
     i=0;
 
-function checkExistingKeyframe(keyframe){
+function checkIfKeyframeExist(keyframe){
     i++;
     return keyframe[0]===frame;
 }
-//to nie działa
+
 function findPreviousKeyframe(keyframe){
     i++;
-    return keyframe[0]>frame;
-}
 
-// chce dodać keyframe
-// jeśli keyfreame istnieje, to edytować, jeśli nie, to stworzyć nowy
-// jeśli tworzyć nowy, to jeśli ostatni keyframe jest z klatki większej niż dodawany keyframe, to trzeba znaleźć pierwszą wartość mniejszą niż dodawany keyframe i wstawić go przed nią
-// jeśli taki nie istnieje, to dac jako pierwszą klatkę
-// jeśli ostatni keyframe jest z klatki mniejszej niż dodawany keyframe, to dodać na końcu
+    return keyframe[0]>frame;
+
+}
 
 function addKeyframe(){
     if(objectMode===false) {
         i=0;
-        if(keyframes.find(checkExistingKeyframe)) {
+        if(keyframes.find(checkIfKeyframeExist)) {
             keyframes[i-1]=[frame, ctx.getImageData(0, 0, canvasW, canvasH)];
         }else if(keyframes.length>0) {
             if(keyframes[keyframes.length - 1][0]<frame){
@@ -50,19 +46,35 @@ function addKeyframe(){
         console.log(keyframes);
         lastKeyframe = keyframes.length - 1;
     }else{
-
+        //obiektowa wersja dodawania klatek
     }
 }
-//To nie działa
+
 function setFrame(){
     console.log("Frame set");
-    for(let i=0;i<keyframes.length;i++){
-        if(keyframes[i][0]===frame){
+    let found=false;
+    //sprawdź czy na nowej klatkce jest klatka kluczowa, jeśli tak, to skorzystaj z niej
+    for(let j=0;j<keyframes.length;j++){
+        if(keyframes[j][0]===frame){
             clear();
-            ctx.putImageData(keyframes[i][1],0,0);
-            lastKeyframe=i;
+            ctx.putImageData(keyframes[j][1],0,0);
+            lastKeyframe=j;
+            found=true;
+            console.log("Znaleziono KLATKE!!!");
+            break;
         }
     }
+    if(!found){
+        //znajdź klatkę kluczową przed obecną klatką
+        i=0;
+        if(keyframes.find(findPreviousKeyframe)){
+            lastKeyframe=i-2;
+        }else{
+            lastKeyframe=keyframes.length-1;
+        }
+
+    }
+
 }
 //to jakby działa?
 function nextFrame(){
@@ -72,6 +84,7 @@ function nextFrame(){
         try {
             if (keyframes[lastKeyframe + 1][0] === frame) {
                 lastKeyframe++;
+                ctx.putImageData(keyframes[lastKeyframe][1],0,0);
                 console.log("jest i keyframe");
             }
         }
@@ -80,10 +93,7 @@ function nextFrame(){
         }
     }else{
         frame=startFrame;
-        lastKeyframe=0;
-        if(keyframes[0][0]===frame){
-            console.log("Na klatce początkowej jest keyframe")
-        }
+        setFrame();
     }
     document.getElementById("frame").value = frame;
 }
@@ -91,38 +101,35 @@ function nextFrame(){
 function prevFrame(){
     if(frame>startFrame)
     {
-        frame--;
-        document.getElementById("frame").value = frame;
-        if (lastKeyframe > 0 && keyframes[lastKeyframe][0] === frame) {
-            clear();
-            lastKeyframe--;
-            ctx.putImageData(keyframes[lastKeyframe][1], 0, 0);
-
-        }
-
-    }
-}
-function cycleFrames(){
-    if(frame<=endFrame){
-        frame++;
-        if (lastKeyframe < keyframes.length && frame < keyframes.length) {
-            if (keyframes[lastKeyframe + 1][0] === frame) {
-                clear();
-                ctx.putImageData(keyframes[lastKeyframe + 1][1], 0, 0);
-                lastKeyframe++;
+        frame--
+        try {
+            if (keyframes[lastKeyframe - 1][0] === frame) {
+                lastKeyframe--;
+                ctx.putImageData(keyframes[lastKeyframe][1],0,0);
+                console.log("jest i keyframe");
             }
         }
+        catch{
+            console.log("nie ma keyframe");
+        }
     }else{
-        frame=startFrame;
+        frame=endFrame;
         setFrame();
     }
+    document.getElementById("frame").value = frame;
+}
+
+function cycleFrames(){
+    nextFrame();
     console.log("frameCycledto: ",frame);
     document.getElementById("frame").value = frame;
 }
+
 function play(){
     stop();
     playTimer=setInterval(cycleFrames,1000/framerate);
 }
+
 function stop(){
     clearInterval(playTimer);
 }
