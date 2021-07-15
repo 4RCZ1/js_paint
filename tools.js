13e7dfbb01aed53be5f9b766c84fe9b53deb7533
4RCZ1 <4RCZ1@users.noreply.github.com>
2021-07-15 18:48:45 +0200

    fixed brush

--- tools.js
+++ tools.js
@@ -16,11 +16,13 @@ function brushStart(){
     clearInterval(brushTimer);
     if(brushMode){
         console.log('brushStart');
+        ctx.beginPath();
         ctx.moveTo(mouseX,mouseY);
         brushTimer=setInterval(function(){ ctx.lineTo(mouseX,mouseY); ctx.stroke();},1);
     }
 }
 function brushStop(){
     console.log('brushStop');
+    ctx.stroke();
     clearInterval(brushTimer);
 }
