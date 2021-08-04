30cbbfe23b08ca956641c9c12fa6db09c9dce1f8
4RCZ1 <4RCZ1@users.noreply.github.com>
2021-08-04 14:20:24 +0200

    added gradient (early version)

--- tools.js
+++ tools.js
@@ -10,9 +10,6 @@ function brush() {
     fill=true;
 }
 function brushStart(){
-    // if(brushMode) {
-    //     brushTimer = setInterval(placeObject, 10);
-    // }
     clearInterval(brushTimer);
     if(brushMode){
         ctx.beginPath();
@@ -23,4 +20,34 @@ function brushStart(){
 function brushStop(){
     ctx.stroke();
     clearInterval(brushTimer);
+}
+function gradient(){
+    clearInterval(placing);
+    let grd=ctxCursor.createLinearGradient(0, 20, 200, 20);
+    let startPin=new Pinpoint(0,20);
+    let endPin=new Pinpoint(200,20);
+    placing=setInterval(function(){gradientPlacer(startPin,endPin,grd);},10);
+}
+function gradientPlacer(startPin,endPin,grd){
+    startPin.draw();
+    endPin.draw();
+    startPin.followOnClick();
+    endPin.followOnClick();
+    grd=ctxCursor.createLinearGradient(startPin.position()[0], startPin.position()[1], endPin.position()[0], endPin.position()[1]);
+    grd.addColorStop(0, document.getElementById("fillColor").value);
+    grd.addColorStop(1, ctxCursor.strokeStyle);
+    console.log(startPin.position()[0],startPin.position()[1]);
+    if(startPin.following){
+        startPin.modify(mouseX,mouseY);
+        clear("cursor");
+    }else if(endPin.following){
+        endPin.modify(mouseX,mouseY);
+        clear("cursor");
+    }
+    //grd.addColorStop(0, "#000000");
+    //grd.addColorStop(1, ctxCursor.strokeStyle);
+    ctxCursor.fillStyle = grd;
+    ctxCursor.fillRect(0, 0, 500, 500);
+    startPin.draw();
+    endPin.draw();
 }
